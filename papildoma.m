687c8e55b7f802945cf701873ca392814b543eaf
OvidijusGrigaliunas <104835827+OvidijusGrigaliunas@users.noreply.github.com>
2023-11-23 19:51:20 +0200

    Update papildoma.m

--- papildoma.m
+++ papildoma.m
@@ -2,57 +2,59 @@ close all
 clear
 clc
 % 1. Duomenų paruošimas
-X = 0.1:1/22:1;
-% for H = 1:19
-% for K = 1:19
+X = 0.1:0.05:4;
 Y =  (1 + 0.6*sin(2*pi*X/0.7)) + 0.3*sin(2*pi*X)/2;
-step = 0.1;
-w = randn(3,1);
+step = 0.01;
+rc_kiekis = 6;
+w = randn(rc_kiekis,1);
+b = randn(1);
 j = 2;
-
-for i=1:2
+for i=1:rc_kiekis
     while Y(j) > Y(j-1)
         c(i) = X(j);
         j=j+1;
     end
     a = j;
-    if i < 2
+    if i < rc_kiekis
          % preleidzia vietas kur mazeja y
         while Y(j) < Y(j-1)
-            j=j+1;
-            b = j;
+            j = j + 1;
         end
-
+        b = j;
         r(i) =  X(round((a+b)/2)) - X(a);
     else
-
         r(i) =  X(a) - X(round((a+b)/2));
-    end
-end
-y_apskaiciuota = zeros(1,length(X));
-for k = 1:100000
-    for j = 1:length(X)
-       
-        % 1 sluosknis
-        %y(i) = 1/(1+exp(-v(i,1)));
-        f1 = gauss(X(j),c(1), r(1));
-        f2 = gauss(X(j),c(2), r(2));
-        y = f1*w(1) + f2*w(2) + w(3);
-        e = Y(j) - y;
-        w(1) = w(1) + step*e*f1;
-        w(2) = w(2) + step*e*f2;
-        w(3) = w(3)+ step*e;
-        % Koeficientu atnaujinimas
-
     end
 end
 r
 c
-X2 = 0:0.005:1;
+y_apskaiciuota = zeros(1,length(X));
+for k = 1:500000
+    for j = 1:length(X)
+       
+        % 1 sluosknis
+        %y(i) = 1/(1+exp(-v(i,1)));
+        for i=1:rc_kiekis
+            f(i) = gauss(X(j),c(i), r(i));
+            fw(i) = f(i)*w(i);
+        end
+        y = sum(fw) + b;
+        e = Y(j) - y;
+        for i=1:6
+            w(i) = w(i) + step*e*f(i);
+        end
+        b = b + step*e;
+        % Koeficientu atnaujinimas
+
+    end
+end
+X2 = 0:0.005:4;
 for j = 1:length(X2)
-        f1 = gauss(X2(j),c(1), r(1));
-        f2 = gauss(X2(j),c(2), r(2));
-        y_apskaiciuota(j) = f1*w(1) + f2*w(2)+w(3);
+        for i=1:rc_kiekis
+            f(i) = gauss(X2(j),c(i), r(i));
+            fw(i) = f(i)*w(i);
+        end
+        y_apskaiciuota(j) = sum(fw) + b;
 end
 figure
 plot(X,Y)
